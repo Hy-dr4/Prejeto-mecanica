d5d6b3dd34418d1bd4967dae3cb2abee834a7b95
Gabrielmktt <gabrielbispodeabrantes1212@gmail.com>
2026-06-01 15:51:28 -0300

    adiciona arquivo index.php com inclusão de seções principais do site

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'includes/Header.php';
+require_once  'sections/hero.php';
+require_once 'sections/servicos.php';
+require_once 'sections/diferenciais.php';
+require_once 'sections/sobre.php';
+require_once 'sections/contato.php';
+require_once 'includes/footer.php';
+?>
