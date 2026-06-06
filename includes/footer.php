f56685030240a4d8c1b304c50d04a5b8942ff72a
Gabrielmktt <gabrielbispodeabrantes1212@gmail.com>
2026-06-06 20:28:26 -0300

    ajusta a seção de rodapé com novas colunas e padding, além de corrigir a formatação do arquivo de contato

--- includes/footer.php
+++ includes/footer.php
@@ -2,108 +2,110 @@
 
     <div class="footer-container">
 
-        <!-- Empresa -->
-        <div class="footer-col">
+                <!-- Empresa -->
+                <div class="footer-col">
 
-            <h3 class="footer-logo">Oficina Premium</h3>
+                    <h3 class="footer-logo">Oficina Premium</h3>
 
-            <p>
-                Especialistas em veículos nacionais e importados há mais de 15 anos.
-                Tecnologia, qualidade e confiança em cada serviço.
-            </p>
+                    <p>
+                        Especialistas em veículos nacionais e importados há mais de 15 anos.
+                        Tecnologia, qualidade e confiança em cada serviço.
+                    </p>
 
-            <div class="social-links">
+                    <div class="social-links">
 
-                <a href="#">
-                    <i class="fa-brands fa-whatsapp"></i>
-                </a>
+                        <a href="#">
+                            <i class="fa-brands fa-whatsapp"></i>
+                        </a>
 
-                <a href="#">
-                    <i class="fa-brands fa-instagram"></i>
-                </a>
+                        <a href="#">
+                            <i class="fa-brands fa-instagram"></i>
+                        </a>
 
-                <a href="#">
-                    <i class="fa-brands fa-facebook-f"></i>
-                </a>
+                        <a href="#">
+                            <i class="fa-brands fa-facebook-f"></i>
+                        </a>
 
-                <a href="#">
-                    <i class="fa-brands fa-youtube"></i>
-                </a>
+                        <a href="#">
+                            <i class="fa-brands fa-youtube"></i>
+                        </a>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            <footer class="footer">
+                <!-- Links -->
+                <div class="footer-col">
+
+                    <h4>Links Rápidos</h4>
+
+                    <ul>
+
+                        <li><a href="#servicos">Nossos Serviços</a></li>
+
+                        <li><a href="#sobre">Sobre Nós</a></li>
+
+                        <li><a href="#contato">Contato</a></li>
+
+                    </ul>
+
+                </div>
+
+                <!-- Serviços -->
+                <div class="footer-col">
+
+                    <h4>Principais Serviços</h4>
+
+                    <ul>
+
+                        <li>Manutenção Preventiva</li>
+
+                        <li>Motor e Câmbio</li>
+
+                        <li>Sistema Elétrico</li>
+
+                        <li>Ar Condicionado</li>
+
+                        <li>Veículos Importados</li>
+
+                    </ul>
+
+                </div>
+
+                <!-- Contato -->
+                <div class="footer-col">
+
+                    <h4>Contato</h4>
+
+                    <ul>
+                        <li>
+                            <i class="fa-solid fa-phone"></i>
+                            (11) 99999-9999
+                        </li>
+
+                        <li>
+                            <i class="fa-solid fa-location-dot"></i>
+                            Rua Exemplo, 123
+                        </li>
+
+                        <li>
+                            <i class="fa-solid fa-envelope"></i>
+                            contato@empresa.com.br
+                        </li>
+
+                        <li>
+                        <i class="fa-regular fa-clock"></i>
+                            Seg-Sex: 08h às 18h
+                        </li>
+
+                    </ul>
 
                 </div>
 
             </div>
-
-        </div>
-
-        <!-- Links -->
-        <div class="footer-col">
-
-            <h4>Links Rápidos</h4>
-
-            <ul>
-
-                <li><a href="#servicos">Nossos Serviços</a></li>
-
-                <li><a href="#sobre">Sobre Nós</a></li>
-
-                <li><a href="#contato">Contato</a></li>
-
-            </ul>
-
-        </div>
-
-        <!-- Serviços -->
-        <div class="footer-col">
-
-            <h4>Principais Serviços</h4>
-
-            <ul>
-
-                <li>Manutenção Preventiva</li>
-
-                <li>Motor e Câmbio</li>
-
-                <li>Sistema Elétrico</li>
-
-                <li>Ar Condicionado</li>
-
-                <li>Veículos Importados</li>
-
-            </ul>
-
-        </div>
-
-        <!-- Contato -->
-        <div class="footer-col">
-
-            <h4>Contato</h4>
-
-            <ul>
-                <li>
-                     <i class="fa-solid fa-phone"></i>
-                    (11) 99999-9999
-                 </li>
-
-                <li>
-                     <i class="fa-solid fa-location-dot"></i>
-                    Rua Exemplo, 123
-                </li>
-
-                <li>
-                    <i class="fa-solid fa-envelope"></i>
-                    contato@empresa.com.br
-                </li>
-
-                <li>
-                   <i class="fa-regular fa-clock"></i>
-                    Seg-Sex: 08h às 18h
-                </li>
-
-            </ul>
-
-        </div>
-
     </div>
 
     <div class="footer-bottom">
