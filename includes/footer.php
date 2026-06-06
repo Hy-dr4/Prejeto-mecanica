4ead5af85eae7cd059144f2eb3982fb3933da217
Gabrielmktt <gabrielbispodeabrantes1212@gmail.com>
2026-06-06 20:31:42 -0300

    refatora a estrutura do rodapé, consolidando colunas e melhorando a formatação

--- includes/footer.php
+++ includes/footer.php
@@ -1,133 +1,59 @@
 <footer class="footer">
-
     <div class="footer-container">
-
-                <!-- Empresa -->
-                <div class="footer-col">
-
-                    <h3 class="footer-logo">Oficina Premium</h3>
-
-                    <p>
-                        Especialistas em veículos nacionais e importados há mais de 15 anos.
-                        Tecnologia, qualidade e confiança em cada serviço.
-                    </p>
-
-                    <div class="social-links">
-
-                        <a href="#">
-                            <i class="fa-brands fa-whatsapp"></i>
-                        </a>
-
-                        <a href="#">
-                            <i class="fa-brands fa-instagram"></i>
-                        </a>
-
-                        <a href="#">
-                            <i class="fa-brands fa-facebook-f"></i>
-                        </a>
-
-                        <a href="#">
-                            <i class="fa-brands fa-youtube"></i>
-                        </a>
-
-                        </div>
-
-                    </div>
-
-                </div>
-
-            <footer class="footer">
-                <!-- Links -->
-                <div class="footer-col">
-
-                    <h4>Links Rápidos</h4>
-
-                    <ul>
-
-                        <li><a href="#servicos">Nossos Serviços</a></li>
-
-                        <li><a href="#sobre">Sobre Nós</a></li>
-
-                        <li><a href="#contato">Contato</a></li>
-
-                    </ul>
-
-                </div>
-
-                <!-- Serviços -->
-                <div class="footer-col">
-
-                    <h4>Principais Serviços</h4>
-
-                    <ul>
-
-                        <li>Manutenção Preventiva</li>
-
-                        <li>Motor e Câmbio</li>
-
-                        <li>Sistema Elétrico</li>
-
-                        <li>Ar Condicionado</li>
-
-                        <li>Veículos Importados</li>
-
-                    </ul>
-
-                </div>
-
-                <!-- Contato -->
-                <div class="footer-col">
-
-                    <h4>Contato</h4>
-
-                    <ul>
-                        <li>
-                            <i class="fa-solid fa-phone"></i>
-                            (11) 99999-9999
-                        </li>
-
-                        <li>
-                            <i class="fa-solid fa-location-dot"></i>
-                            Rua Exemplo, 123
-                        </li>
-
-                        <li>
-                            <i class="fa-solid fa-envelope"></i>
-                            contato@empresa.com.br
-                        </li>
-
-                        <li>
-                        <i class="fa-regular fa-clock"></i>
-                            Seg-Sex: 08h às 18h
-                        </li>
-
-                    </ul>
-
-                </div>
-
+        
+        <div class="footer-col">
+            <h3 class="footer-logo">Oficina Premium</h3>
+            <p>
+                Especialistas em veículos nacionais e importados há mais de 15 anos.
+                Tecnologia, qualidade e confiança em cada serviço.
+            </p>
+            <div class="social-links">
+                <a href="#"><i class="fa-brands fa-whatsapp"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                <a href="#"><i class="fa-brands fa-youtube"></i></a>
             </div>
+        </div>
+
+        <div class="footer-col">
+            <h4>Links Rápidos</h4>
+            <ul>
+                <li><a href="#servicos">Nossos Serviços</a></li>
+                <li><a href="#sobre">Sobre Nós</a></li>
+                <li><a href="#contato">Contato</a></li>
+            </ul>
+        </div>
+
+        <div class="footer-col">
+            <h4>Principais Serviços</h4>
+            <ul>
+                <li>Manutenção Preventiva</li>
+                <li>Motor e Câmbio</li>
+                <li>Sistema Elétrico</li>
+                <li>Ar Condicionado</li>
+                <li>Veículos Importados</li>
+            </ul>
+        </div>
+
+        <div class="footer-col">
+            <h4>Contato</h4>
+            <ul>
+                <li><i class="fa-solid fa-phone"></i> (11) 99999-9999</li>
+                <li><i class="fa-solid fa-location-dot"></i> Rua Exemplo, 123</li>
+                <li><i class="fa-solid fa-envelope"></i> contato@empresa.com.br</li>
+                <li><i class="fa-regular fa-clock"></i> Seg-Sex: 08h às 18h</li>
+            </ul>
+        </div>
+
+    </div> <div class="footer-bottom">
+        <p>© 2026 Oficina Premium. Todos os direitos reservados.</p>
+        <div class="footer-legal">
+            <a href="#">Política de Privacidade</a>
+            <a href="#">Termos de Uso</a>
+        </div>
     </div>
 </footer>
 
-
-    <div class="footer-bottom">
-
-        <p>
-            © 2026 Oficina Premium. Todos os direitos reservados.
-        </p>
-
-        <div class="footer-legal">
-
-            <a href="#">Política de Privacidade</a>
-
-            <a href="#">Termos de Uso</a>
-
-        </div>
-
-    </div>
-
-
 <script src="assets/js/script.js"></script>
-
 </body>
 </html>
