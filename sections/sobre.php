49e1be20159c3fee9d82bea05cd58b67310aee39
Gabrielmktt <gabrielbispodeabrantes1212@gmail.com>
2026-06-06 18:57:21 -0300

    Refactor code structure for improved readability and maintainability

--- sections/sobre.php
+++ sections/sobre.php
@@ -1,37 +1,51 @@
 <section class="sobre"  id="sobre">
 
-    <img src="assets/img/sobre.jpg" alt="Imagem de um mecânico trabalhando em um carro, simbolizando a dedicação e expertise da oficina.">
-    <span class="tag">
-        SOBRE NÓS
-    </span>
-    <h2>
-        Tradição e Inovação em Cada Serviço
-    </h2>
-    <p>
-        Há mais de 15 anos, nossa oficina se dedica a oferecer o melhor atendimento automotivo da região, combinando experiência tradicional com tecnologia de ponta.
-    </p>
-    <p>
-        Atendemos desde veículos populares até modelos premium e importados, sempre com o mesmo compromisso: qualidade excepcional, transparência total e a confiança que você e sua família merecem.
-    </p>
-    <div class="info-sobre">
+    <!-- LADO ESQUERDO -->
+        <div class="sobre-imagem">
+                <img src="assets/img/sobre.jpg" 
+                alt="Mecânico trabalhando">
+            </div>
+            <!-- LADO DIREITO -->
+            <div class="sobre-conteudo">
 
-             
+            <span class="tag">
+                SOBRE NÓS
+            </span>
+                <h2>
+                    Tradição e Inovação em Cada Serviço
+                </h2>
+            <p>
+                Há mais de 15 anos, nossa oficina se dedica a oferecer o melhor atendimento automotivo da região, combinando experiência tradicional com tecnologia de ponta.
+            </p>
+
+            <p>
+                Atendemos desde veículos populares até modelos premium e importados, sempre com o mesmo compromisso: qualidade excepcional, transparência total e a confiança que você e sua família merecem.
+            </p>
+
+            <div class="item-sobre">
                 <div class="icone-sobre">
                     <i class="fa-solid fa-clock"></i>
                 </div>
-            <h3>Certificados</h3>
-            <p>Técnicos certificados e treinamento contínuo</p>
-        </div>
+                <div>
+                    <h3>Certificados</h3>
+                    <p>Técnicos certificados e treinamento contínuo</p>
+                </div>
+            </div>
 
-       <div class="icone-sobre">
+            <div class="item-sobre">
+                <div class="icone-sobre">
                     <i class="fa-solid fa-shield"></i>
                 </div>
-            <h3>Equipe Especializada</h3>
-            <p>Profissionais com anos de experiência</p>
+                <div>
+                    <h3>Equipe Especializada</h3>
+                    <p>Profissionais com anos de experiência</p>
+                </div>
+            </div>
         </div>
-</div>
+    </div>
 
 </section>
+
 
 
 
