0f494057b5167454311b5d3e1c373c32b8f54dbf
Gabrielmktt <gabrielbispodeabrantes1212@gmail.com>
2026-06-06 21:17:31 -0300

    corrige link do WhatsApp removendo o caractere extra

--- sections/servicos.php
+++ sections/servicos.php
@@ -53,7 +53,7 @@
     </div>
 
     <p>Não encontrou o serviço que procura?</p>
-    <a href="#https://api.whatsapp.com/send/?phone=5500000000000&text&type=phone_number&app_absent=0" 
+    <a href="https://api.whatsapp.com/send/?phone=5500000000000&text&type=phone_number&app_absent=0" 
     class="btn">
         Fale com nossos especialistas
     </a>
